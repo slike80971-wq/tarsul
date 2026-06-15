a77141e73f20d7463a80bfe6f7a15637d2b64aeb
slike80971-wq <slike80971@gmail.com>
2026-06-16 00:03:01 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+}
 
-const nextConfig: NextConfig = {
-    // output: "standalone",  // ← عطّل هذا لـ Vercel | فعّله لـ Docker,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: false,
-  allowedDevOrigins: [
-    "127.0.0.1",
-    "localhost",
-    "21.0.17.147",
-    ".space-z.ai",
-  ],
-};
-
-export default nextConfig;
+module.exports = nextConfig
