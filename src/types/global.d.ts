10eccf632fb26ddd2626d7b1afbce07eee04fbca
slike80971-wq <slike80971@gmail.com>
2026-06-23 02:48:32 +0300

    s

--- src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    DATABASE_URL: string;
-    NEXTAUTH_URL: string;
-    NEXTAUTH_SECRET: string;
-    NEXT_PUBLIC_SOCKET_URL?: string;
-    NODE_ENV: 'development' | 'production' | 'test';
-  }
-}
